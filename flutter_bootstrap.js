fe1cd3bc0cf50af3d4e53dd7086af48c47998f79
Genau <genaujunior@gmail.com>
2025-10-24 02:46:49 +0000

    🚀 Deploy Yoobe CRM with splash screen and email settings

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"c29809135135e262a912cf583b2c90deb9ded6
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1743966868"
+    serviceWorkerVersion: "3898492420"
   }
 });
